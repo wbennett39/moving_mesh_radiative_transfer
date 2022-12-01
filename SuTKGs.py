aabd9ece557e158dfe25124eb2df47b3708a8325
William Bennett <wbennet2@nd.edu>
2022-11-30 19:41:35 -0500

    -

--- SuTKGs.py
+++ SuTKGs.py
@@ -45,7 +45,9 @@ run.load('su_olson_thick')
 for count, t in enumerate(t_list):
     run.parameters['all']['tfinal'] = t
     # run.mesh_parameters['sqrt_t_move_factor'] = factor_list[count]
+    run.parameters['all']['N_spaces'][0] = 128
     run.parameters['gaussian_source']['x0'][0] = factor_list[count]
+    run.parameters['all']['Ms'][0] = 10
     run.gaussian_source(False, False)
     plt.close()
     plt.close()
