d3e9f4a229958a1fa7f197523745ae582ea8b394
William Bennett <wbennet2@nd.edu>
2022-12-05 07:29:59 -0500

    -

--- NLTKGauss.py
+++ NLTKGauss.py
@@ -36,9 +36,9 @@ plt.close()
 # t_list = [0.1, 0.31623, 1.0, 3.16228, 10.0, 31.6228, 100.0]
 # t_list = [0.1, 0.31623, 1, 3.16228, 10.0]
 t_list = [0.3, 3.0, 30.0]
-# factor_list = [2.5, 3.0, 6.0] # for thick gaussians nonlinear
+factor_list = [2.5, 3.0, 6.0] # for thick gaussians nonlinear
 # factor_list = [0.8, 0.9, 1.2]
-factor_list = [0.6, 0.6, 1.0, 1.0, 1.6, 1.6, 1.5] # thin gaussian ninlinear
+# factor_list = [0.6, 0.6, 1.0, 1.0, 1.6, 1.6, 1.5] # thin gaussian ninlinear
 
 run.load('rad_transfer_const_cv_thick')
 
